fd89531fb466bc2ef193593cb12c81002cd1175c
Chikaoji <chikaoji88@gmail.com>
2022-06-30 22:47:58 -0700

    My fourth C programing practice

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+* main-Entry piont
+* Print word to ouptut
+* Return:0 (Sucess) 
+*/
+#include <stdio.h>
+int main(void) 
+{
+	puts("programming is like building a multiingual puzzle/n"); 
+	return (0);    
+} 	
